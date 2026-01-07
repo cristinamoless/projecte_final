16cd6d4ad3ae3e298d974cc495d1fb3f8eaaf208
Ingrid <ingrid.lara@estudiantat.upc.edu>
2026-01-07 19:01:15 +0100

    walk backwards

--- Assets/Scripts/WalkingAnimation.cs
+++ Assets/Scripts/WalkingAnimation.cs
@@ -7,16 +7,30 @@ public class WalkingAnimation : MonoBehaviour
 
     void Start()
     {
-        _animator = GetComponent<Animator>();
         _control = GetComponent<PlayerControl>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        Vector3 horizontalVelocity = _control._lastVelocity;
-        horizontalVelocity.y = 0f;
+        Vector3 velocity = _control._lastVelocity;
+        velocity.y = 0f;
 
-        bool isWalking = horizontalVelocity.magnitude > 0.1f;
+        float speed = velocity.magnitude;
+        bool isWalking = speed > 0.1f;
+
+        bool isBackward = false;
+
+        if (isWalking)
+        {
+            Vector3 moveDir = velocity.normalized;
+            float dot = Vector3.Dot(transform.forward, moveDir);
+
+            // dot < 0 = enrere
+            isBackward = dot < -0.1f;
+        }
+
         _animator.SetBool("isWalking", isWalking);
+        _animator.SetBool("isBackward", isBackward);
     }
 }
