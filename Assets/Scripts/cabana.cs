4af62c4c796f350cd698d75c98f1317ddca235d0
albertrodriguezd <albert.rodriguez.domingo@estudiantat.upc.edu>
2026-01-10 20:04:20 +0100

    canvicabana

--- Assets/Scripts/cabana.cs
+++ Assets/Scripts/cabana.cs
@@ -2,23 +2,13 @@ using UnityEngine;
 
 public class cabana : MonoBehaviour
 {
-    public WorldManager _world;
     private Vector3 baseScale;
 
     void Start()
     {
         baseScale = transform.localScale;
-    }
-    private void OnEnable()
-    {
         WorldManager.OnBetterWorld += canviEscala;
         WorldManager.OnWorseWorld += canviEscala;
-    }
-
-    private void OnDisable()
-    {
-        WorldManager.OnBetterWorld -= canviEscala;
-        WorldManager.OnWorseWorld -= canviEscala;
     }
 
     private void canviEscala(WorldManager wm)
@@ -26,4 +16,5 @@ public class cabana : MonoBehaviour
         transform.localScale = (baseScale * wm.WorldState);
     }
 }
+
 
